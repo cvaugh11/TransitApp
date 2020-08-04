c2729f3573d02ed39ff4b8967c1eb260024032a1
Carlton Vaughn <RichHomieVaughn@carltons-mbp.home>
2020-08-04 16:07:36 -0400

    first commit

---
+++ src/main/java/com/tts/transitapp/model/Element.java
@@ -0,0 +1,9 @@
+package com.tts.transitapp.model;
+
+import ch.qos.logback.core.util.Duration;
+
+public class Element {
+    public Distance distance;
+    public Duration duration;
+    public String status;
+}
